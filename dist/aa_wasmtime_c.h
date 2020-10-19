61c8368a41b30a635b8061152471366f0cdb2e7b
Benedict Gaster <benedict.gaster@gmail.com>
2020-10-19 17:45:33 +0100

    Added compute function for 0 in 2 out non interlaced

--- dist/aa_wasmtime_c.h
+++ dist/aa_wasmtime_c.h
@@ -43,4 +43,6 @@ int aa_module_get_number_outputs(AAModule *ptr);
 
 void aa_module_compute_zero_one(AAModule *ptr, int frames, float *outputs);
 
+void aa_module_compute_zero_two_non(AAModule *ptr, int frames, float *output0, float *output1);
+
 } // extern "C"
